5d91627726b892e1826fe9492cd8e364275330ba
will henrique <willhc1984@gmail.com>
2022-06-09 13:35:00 -0300

    Interface comparable

---
+++ src/interface_comparable/Employee.java
@@ -0,0 +1,37 @@
+package interface_comparable;
+
+public class Employee implements Comparable<Employee>{
+	
+	private String name;
+	private double salary;
+	
+	public Employee(String name, double salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int compareTo(Employee other) {
+		return name.compareTo(other.getName());
+	}
+
+	
+
+}
