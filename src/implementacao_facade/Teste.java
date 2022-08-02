6b4a59a2cfd3c6a0a1c4b20efc6b160e5b156403
unknown <willhc1984@gmail.com>
2022-08-02 16:24:49 -0300

    Implementação facade

---
+++ src/implementacao_facade/Teste.java
@@ -0,0 +1,13 @@
+package implementacao_facade;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Facade f1 = new Facade();
+		f1.migrarCliente("William", "18120-000");
+		
+	
+	}
+
+}
